a1e97e8d7de61835faf64c2a90f263af8e2ab494
stswoon <stswoon@yandex.ru>
2017-08-13 20:16:04 +0400

    STONE: simple project

--- frontend/public/bundle.js
+++ frontend/public/bundle.js
@@ -1,6 +1,12 @@
 "use strict";
 
-fetch("https://stswoon-fm-gateway.herokuapp.com/backend/hello/me").then(function(response) {
-   console.log(response);
-   alert("See console");
-});
+fetch("https://stswoon-fm-gateway.herokuapp.com/backend/hello/me")
+    .then(function (response) {
+        console.log(response);
+        return response.text();
+        // console.lresponse.json()
+    })
+    .then(function (data) {
+        console.log(data);
+        alert("See console. Anr response is '" + data + "'");
+    });
--- gateway/src/main/java/nodomain/stswoon/financemanager/gateway/ZuulServerApplication.java
+++ gateway/src/main/java/nodomain/stswoon/financemanager/gateway/ZuulServerApplication.java
@@ -3,9 +3,13 @@ package nodomain.stswoon.financemanager.gateway;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @ComponentScan
@@ -13,7 +17,25 @@ import org.springframework.stereotype.Controller;
 @Controller
 @EnableZuulProxy
 public class ZuulServerApplication {
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
-  }
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
